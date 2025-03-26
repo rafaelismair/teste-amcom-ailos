0d71ed0026fbe9eb6e0f42734128883b7676fe3d
Rafael Ismair <rafaelismair@gmail.com>
2025-03-26 20:52:38 -0300

    feat: commit questao 1

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -1,9 +1,43 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace Questao1
 {
-    class ContaBancaria {
+    class ContaBancaria
+    {
+        private const double TAXA_SAQUE = 3.50;
 
-       
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0; 
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial)
+            : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + TAXA_SAQUE;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
     }
 }
